c7ff3656377bc65ea0032d38fc8e3376775953b0
Zeyu.Tian <498442999@qq.com>
2016-09-22 17:10:45 +0800

    yield return

---
+++ SimpleEnumerate/02YieldReturn/IterationSample.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * 利用C#2和yield return来迭代实例集合
+ */
+using System.Collections;
+
+namespace _02YieldReturn
+{
+    public class IterationSample : IEnumerable
+    {
+        object[] values;
+        int startingPoint;
+
+        public IterationSample(object[] values, int startingPoint)
+        {
+            this.values = values;
+            this.startingPoint = startingPoint;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int index = 0; index < values.Length; index++)
+            {
+                yield return values[(index + startingPoint)%values.Length];
+            }
+        }
+    }
+}
---
+++ SimpleEnumerate/02YieldReturn/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02YieldReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] values = { "a", "b", "c", "d", "e" };
+            IterationSample collection = new IterationSample(values, 3);
+            foreach (var x in collection)
+            {
+                Console.WriteLine(x);
+            }
+            Console.Read();
+        }
+    }
+}
